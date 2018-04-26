58f2b926e63dcb151dcd5bb5598b89839dc3b816
Pawel <pplowucha@gmail.com>
2018-04-26 05:59:35 +0200

    lab6 added

---
+++ Lab6/Assets/Own Assets/Scripts/ScoreController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text scoreText;
+	int count = 0;
+    int score;
+
+    // Use this for initialisation
+    void Start()
+    {
+        Refresh();
+    }
+
+    // Update the score.
+    public void Points(short points)
+    {
+        score += points;
+		count++;
+        Refresh();
+    }
+
+    // Reproduce the score text.
+    void Refresh()
+    {
+        scoreText.text = "Points: " + score.ToString();
+		if (count == 10) {
+		
+			scoreText.text = "You Win :) !!!";
+			Time.timeScale = 0;
+		
+		}
+    }
+}
